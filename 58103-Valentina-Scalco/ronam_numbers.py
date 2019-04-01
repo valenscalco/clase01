35c28f687c3ac406c63a4b2d7746427b9cda4e7d
valenscalco <mv.scalco@alumno.um.edu.ar>
2019-04-01 20:09:55 -0300

    V1

--- 58103-Valentina-Scalco/ronam_numbers.py
+++ 58103-Valentina-Scalco/ronam_numbers.py
@@ -16,6 +16,38 @@ def roman_to_decimal(roman_number):
             if ant == 'I':
                 decimal_number = decimal_number - 2
 
+        if letter == 'L':
+            decimal_number = decimal_number + 50
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+
+        if letter == 'C':
+            decimal_number = decimal_number + 100
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+
+        if letter == 'D':
+            decimal_number = decimal_number + 500
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+            if ant == 'C':
+                decimal_number = decimal_number - 200
+
+        if letter == 'M':
+            decimal_number = decimal_number + 1000
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+            if ant == 'C':
+                decimal_number = decimal_number - 200
+
         ant = letter
 
     return decimal_number
--- 58103-Valentina-Scalco/test_roman_numbers.py
+++ 58103-Valentina-Scalco/test_roman_numbers.py
@@ -37,11 +37,59 @@ class TestRomanNumbers(unittest.TestCase):
 
    def test_roman_IX_to_decimal(self):
        decimal_number = roman_to_decimal('IX')
-       self.assertEqual(decimal_number, 9)
+       self.assertEqual(decimal_number, 9 )
 
    def test_roman_X_to_decimal(self):
        decimal_number = roman_to_decimal('X')
        self.assertEqual(decimal_number, 10)
+
+   def test_roman_XXIV_to_decimal(self):
+       decimal_number = roman_to_decimal('XXIV')
+       self.assertEqual(decimal_number, 24)
+
+   def test_roman_XLIII_to_decimal(self):
+       decimal_number = roman_to_decimal('XLIII')
+       self.assertEqual(decimal_number, 43)
+
+   def test_roman_LVIII_to_decimal(self):
+       decimal_number = roman_to_decimal('LVIII')
+       self.assertEqual(decimal_number, 58)
+
+   def test_roman_LXXII_to_decimal(self):
+       decimal_number = roman_to_decimal('LXXII')
+       self.assertEqual(decimal_number, 72)
+
+   def test_roman_LXXXVII_to_decimal(self):
+       decimal_number = roman_to_decimal('LXXXVII')
+       self.assertEqual(decimal_number, 87)
+
+   def test_roman_XCI_to_decimal(self):
+       decimal_number = roman_to_decimal('XCI')
+       self.assertEqual(decimal_number, 91)
+
+   def test_roman_IC_to_decimal(self):
+       decimal_number = roman_to_decimal('IC')
+       self.assertEqual(decimal_number, 99)
+
+   def test_roman_CI_to_decimal(self):
+       decimal_number = roman_to_decimal('CI')
+       self.assertEqual(decimal_number, 101)
+
+   def test_roman_CIL_to_decimal(self):
+       decimal_number = roman_to_decimal('CIL')
+       self.assertEqual(decimal_number, 149)
+
+   def test_roman_CDLXXVIII_to_decimal(self):
+       decimal_number = roman_to_decimal('CDLXXVIII')
+       self.assertEqual(decimal_number, 478)
+
+   def test_roman_DCXLLIII_to_decimal(self):
+       decimal_number = roman_to_decimal('DCXLLIII')
+       self.assertEqual(decimal_number, 693)
+
+   def test_roman__to_decimal(self):
+       decimal_number = roman_to_decimal('CMLIV')
+       self.assertEqual(decimal_number, 954)
 
 if __name__ == '__main__':
    unittest.main()
